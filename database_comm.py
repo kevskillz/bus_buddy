6b05ce4e6602c5cff0a2830a9c413f0353636c09
kevskillz <kevskillz10@gmail.com>
2022-05-20 17:56:00 -0400

    finished logs

--- database_comm.py
+++ database_comm.py
@@ -1,11 +1,12 @@
 import serial
+from datetime import datetime
 import pymongo
 from pymongo import MongoClient
 f = open("pass.txt", "r")
 passw = f.read().rstrip('\n')
 cluster=MongoClient(f'mongodb+srv://kevskillz:{passw}@cluster0.ysqun.mongodb.net/bus_buddy?retryWrites=true&w=majority')
 db = cluster['bus_buddy']
-coll = db['user_data']
+coll = db.time_logs
 
 
 
@@ -16,8 +17,24 @@ if __name__ == '__main__':
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
-            line = line[1:]
+            line = str(line[1:])
             query = coll.find({'id':line})
-            for user in query:
-               print(user['name'])
+            b = True
+            now = datetime.now()
+            dt_string = now.strftime("%Y/%m/%d %H:%M")
+            if coll.count_documents({ 'id': line }, limit = 1) != 0:
+               for log_poss in query:
+                 if log_poss['off_time'] == "Null":
+                    coll.update_one({"_id": log_poss["_id"]}, {"$set": {"off_time": dt_string}})
+                    b = False
+
+            if b:
+                log = {
+                    "id":line,
+                    "off_time":"Null",
+                    "on_time":str(dt_string)
+                 }
+                print("inserting")
+                print(log)
+                coll.insert_one(log)
 
